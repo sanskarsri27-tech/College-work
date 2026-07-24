e236c4cb7e2e291d93b3fa9d5348374f56b48960
Sanskar Srivastava <sanskarsri27@gmail.com>
2026-07-24 09:46:56 +0530

    DAY 2

---
+++ classwork2.py
@@ -0,0 +1,197 @@
+name= "Sanskar"
+age=19
+print("---DETAILS---")
+print(name)
+print(age)
+
+num1=8
+num2=10
+sum= num1+num2
+print(sum)
+
+'''a=int(input("enter your first number\n"))
+b=int(input("enter your second number\n"))
+a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b)'''
+
+'''a=10
+print(type(a))'''
+
+# a= 34.34
+# print(type(a))
+
+# a=[1,2,3,4,5,6,7]
+# print(type(a))
+
+# a= bool(10)
+# print(type(a))
+
+'''a=27
+b=28
+sum=a+b
+print(sum)
+rem=a%b
+print(rem)
+mul=a*b
+print(mul)
+
+if(a==b):
+    print("equal")
+else:
+    print("not equal")
+
+    if(a>100 or b>100):
+        print("number is greater than 100")
+    else:
+        print("not greater than 100")
+
+square1= a*a
+square2= b*b
+print("square is")
+print(square1)
+print(square2)'''
+
+# def abc(name):
+#     print(name)
+# abc("sanskar")
+
+# def length(name):
+#     print("length of your name is\t",  len(name))
+# length("sanskar")
+
+# list=["a","b","c"]
+# print(list)
+# list.append("d")
+# print(list)
+# list.remove("a")
+# print(list)
+# list[2]= "s"
+# print(list)
+
+'''rows=int(input("enter the number of rows"))
+j=1
+for i in range(j, rows+1):
+    print("*"*i)'''
+
+# n=1
+# while(n<=10):
+#     print(n)
+#     n+=1
+
+# for i in range(0,20):
+#     if(i%2==0):
+#         print(i)
+
+# for i in range(1,10):
+#    print("5 *",i,"=", 5*i)
+
+# sum=0
+# for i in range(0,100):
+#     sum = sum+i
+#     print(sum)
+
+#star pattern
+# rows=5
+# for i in range(1, rows+1): 
+#         for j in range(1, i+1): 
+#             print("*", end='') 
+#         print()
+# for i in range(rows-1,0,-1):
+#      for j in range(1,i+1):
+#            print("*",end='')
+#      print()
+
+# # create the list of 5 fruits and print it
+# fruits=["apple","banana","orange","grape","mango"]
+# print(fruits)
+
+# #print the first and last element of the list
+# print(fruits[0])
+# print(fruits[4])   
+# fruits.remove("banana")
+# print(fruits)
+# print(len(fruits))
+
+# list=["kevin","anny"]
+# list.append("ronny")
+# print(list)
+# print(len(list))
+
+#sort a list in ascending order
+# fruits=["apple","banana","orange","grape","mango"]
+# fruits.sort()
+# print(fruits)
+
+ #largest numberin alist
+# numbers=[5, 10, 3, 8, 2]
+# largest=max(numbers)
+# print(largest)
+
+#print every item in a list using for loop
+# for i in fruits:
+#     print(i)
+
+#print the second element in a tupple
+# fruits=("apple","banana","orange","grape","mango")
+# print(fruits[1])
+
+#count how many times 5 appears in a tuple
+# numbers=(1, 2, 3, 4, 5, 5, 6, 7, 5)
+# count=numbers.count(5)
+# print(count)
+
+#convert a tuple into a list
+# tuple=(1,2,3,5,6,7,8,9)
+# abc=list(tuple)
+# print(abc)
+
+#print all elements of a tuple using loop
+# tuple=(1,2,3,4,5,6,7,8,9)
+# for i in tuple:
+#     print(i)
+
+#to print hello python
+# def hello(name):
+#     print(name)
+# hello("HELLO PYTHON")
+
+#function to add two numbers
+# def add(sum):
+#     print(sum)
+# add(10+20)
+
+#write a function to check odd or even number
+# def check(num):
+#     if(num%2==0):
+#         print("even")
+#     else:
+#         print("odd")
+# check(28)
+
+#write a function to calculate average of three numbers
+# def average(a,b,c):
+#     avg=(a+b+c)/3
+#     print(avg)
+# average(10,20,30)
+
+'''dic={"cars":["m340i","m330i","z4"]}
+print(dic)'''
+
+#program to read a file
+# file=open("name.txt","r")
+# print(file.read())
+# file.close()
+
+#function to print square of a number
+# def square(num):
+#     print(num**2)
+# square(5)
+
+#write a function that prints a student's name
+# def student(name):
+#     print(name)
+# student("sanskar")
+
