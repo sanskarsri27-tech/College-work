3150e79281de6fa4d041a4e47f4376a51c6079e6
Sanskar Srivastava <sanskarsri27@gmail.com>
2026-07-24 10:28:53 +0530

    day 4 programs using numpy

---
+++ classwork4.py
@@ -0,0 +1,110 @@
+# import keyword
+# print(keyword.kwlist)
+
+#NUMPY programs
+# import numpy as np
+# a=np.array([20,34,56,78])
+# print(a)
+
+#1D array (performing all the funcitons)
+# import numpy as np
+# a = np.array([20, 34, 56, 78])
+# print("Array is:", a)
+# print("Dimension is:", a.ndim)
+# print("size of the array is:",a.size)
+# print("type of the array is:", a.dtype)
+# print("shape of the array is:", a.shape)
+
+
+
+#2D array(with all functions)
+
+# b=np.array([
+# [27,28,17,2],
+# [23,4,5,6]])
+# print("Array is:",b)
+# print("Dimension is:", b.ndim)
+# print("type of the array is:",b.dtype)
+# print("size of the array is:",b.size)
+# print("shape of the array is:",b.shape)
+
+#3D array (indexing)
+
+# c= np.array([[
+#     [23,4,5,6,7],
+#     [1,2,3,4,5],
+#     [2,3,4,5,6]
+# ]])
+# print("Array is:",c)
+# print(c[0,0,0])
+# print(type(c))
+
+#unity matrix
+# a=np.ones((5,5))
+# print(a)
+
+# #identity matrix
+# a=np.eye(4)
+# print("\n",a)
+
+#empty matrix
+# a=np.empty((1,1))
+# print(a)
+
+#some other functions of numpy
+# a=np.arange(1,7)
+# c=a.reshape(2,3)
+# print(c)
+
+
+# a=np.array([[1,2],[3,4]])
+# b=a.flatten()
+# print(b)
+
+# a=np.array([1,2,3,4,5])
+# print(a[1:])
+
+# a=np.array([1,2,3])
+# b=np.array([27,28,17])
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a**2)
+
+#numpy array containing numbers from 1 to 20
+# a=np.arange(1,21)
+# print(a)
+
+# a=np.((4,4))
+# print(a)
+
+# a=np.eye((5))
+# print(a)
+
+# a=np.ones((2,5))
+# print(a)
+
+# a=np.array([20,45,55,60])
+# b=a[a>50]
+# print(b)
+
+# a=np.arange(1,13)
+# b=a.reshape(3,4)
+# print(b)
+
+# a=int(input("enter your number\t"))
+# ch=input("enter your symbol\t")
+# b=int(input("enter your second number\t"))
+
+# if(ch=="+"):
+#     print(a+b)
+# elif(ch=="-"):
+#     print(a-b)
+# elif(ch=="/"):
+#     print(a/b)
+# elif(ch=="%"):
+#     print(a%b)
+# else:
+#     print("invalid operator")
+
+
